555cc0c8a90da457f6bb60307548126898e28510
hauckc21 <hauck.chris@gmail.com>
2020-11-15 16:25:43 -0600

    url and markers

---
+++ static/js/logic.js
@@ -0,0 +1,8 @@
+//define urls
+var api_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+
+//define markers for map
+function marketSize(magnitude) {
+    return magnitude * 4;
+};
+
