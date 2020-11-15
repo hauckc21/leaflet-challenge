30c454f652666e3afca5b2d3e100a37a9e4537bd
hauckc21 <hauck.chris@gmail.com>
2020-11-15 16:34:52 -0600

    api calls started

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,3 +6,20 @@ function marketSize(magnitude) {
     return magnitude * 4;
 };
 
+//api call to get geoson data
+d3.json(api_quakes, function(geoJson) {
+    L.geoJSON(geoJson.features, {
+        pointToLayer: function(geoJsonPoint, latlng) {
+            return L.circleMarket(latlng, {radius: markerSize(geoJsonPoint.properties.mag) });
+        },
+
+        style: function(geoJsonFeature) {
+            return {
+                fillColor: Color(geoJsonFeature.properties.mag),
+                fillOpacity: 0.7,
+                weight: 0.1,
+                color: "black"
+
+            }
+        },
+
