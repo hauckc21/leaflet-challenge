4f884d036583d9d6a08dcf221d7a09ece24ede52
hauckc21 <hauck.chris@gmail.com>
2020-11-23 14:25:10 -0600

    new map/token

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiaGF1Y2tjIiwiYSI6ImNrZnkzMDIyazF6ZWYycm10MTYxNGdxOWQifQ.HOOSVyjfl9NHhChgPB1YWg";
+const API_KEY = "pk.eyJ1IjoiaGF1Y2tjIiwiYSI6ImNraHV6dGVmazA2dGQzMW1vZGh5d3NybTAifQ.mmDcfDE8B-oYlEjfZFBPgA";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,10 @@
 console.log("this is logic.js");
 
-const API_KEY = "pk.eyJ1IjoiaGF1Y2tjIiwiYSI6ImNrZnkzMDIyazF6ZWYycm10MTYxNGdxOWQifQ.HOOSVyjfl9NHhChgPB1YWgeyJ1IjoiY29sbGVlbjU0NyIsImEiOiJja2Z5YzFrc2ExbDBpMzFxcWc4NHpsZ2ZtIn0.etuB1olIeSofH9wCvn6aPA";
+const API_KEY = "pk.eyJ1IjoiaGF1Y2tjIiwiYSI6ImNraHV6dGVmazA2dGQzMW1vZGh5d3NybTAifQ.mmDcfDE8B-oYlEjfZFBPgA";
 
-// Get data url
-// url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-var api_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
 
-var gray = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var grayMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -16,7 +14,7 @@ var gray = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
   });
  
 
-  var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -25,3 +23,26 @@ var gray = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
     accessToken: API_KEY
   });
 
+  var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+  });
+
+// Create Base Map
+var base_map={
+    GrayMap: grayMap,
+    Satellite: satelliteMap,
+    Outdoors :outdoorsMap
+};
+
+// Declare Map Object & set it to Map Element in the DOM
+var myMap = L.map("mapid",{
+    center: [40.7, -94.5],
+    zoom: 2,
+    layers: [grayMap, satelliteMap, outdoorsMap]
+});
+grayMap.addTo(myMap);
